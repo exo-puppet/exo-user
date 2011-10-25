a5aeafdcc0c95514c2c96cbb41502c0327225168
Arnaud Héritier <aheritier@apache.org>
2011-10-26 01:02:02 +0200

    Add sudo deployment

--- manifests/virtual.pp
+++ manifests/virtual.pp
@@ -10,6 +10,10 @@ class user::virtual {
 				gid     =>   5001,
 				ensure => "present" ;
 		} 
+	 	group {	"swfadmins":
+				gid     =>   5002,
+				ensure => "present" ;
+		} 
         define sysadmin ($uid,$gid,$pass,$realname="",$email="",$sshkey="",$ensure="present") {
 			 	group {	$title:
 						gid     =>      $gid,
@@ -26,6 +30,58 @@ class user::virtual {
                         password =>     $pass,
                         managehome =>   true,
                         require => [Group["$title"],Group["sysadmins"],] ;
+                }
+			 	file {
+					"/home/$title" :
+						ensure => directory,
+						require => User["$title"],
+						owner => "$title",
+						group => "$title" ;
+				}
+                if ( $email != "" ) {
+				 	file {
+						"/home/$title/.forward" :
+							ensure => file,
+							content => "$email",
+							require => User["$title"],
+							owner => "$title",
+							group => "$title" ;
+					}                	
+                }
+                if ( $sshkey != "" ) {
+					ssh_authorized_key { $title:
+					    ensure  =>      "present",
+					    type    =>      "ssh-rsa",
+					    key     =>      "$sshkey",
+					    user    =>      "$title",
+					    require =>      [User["$title"],File["/home/$title/.ssh"]],
+					    name    =>      "$title",
+					}
+					file {
+						"/home/$title/.ssh" :
+							ensure => directory,
+							require => File["/home/$title"],
+							owner => "$title",
+							group => "$title" ;
+					}
+                }
+        }
+        define swfadmin ($uid,$gid,$pass,$realname="",$email="",$sshkey="",$ensure="present") {
+			 	group {	$title:
+						gid     =>      $gid,
+						ensure => $ensure ;
+				} 
+                user { $title:
+                        ensure  =>      $ensure,
+                        uid     =>      $uid,
+                        gid     =>      $gid,
+                        groups  =>      ["swfadmins","sysadmins"],
+                        shell   =>      "/bin/bash",
+                        home    =>      "/home/$title",
+                        comment =>      $realname,
+                        password =>     $pass,
+                        managehome =>   true,
+                        require => [Group["$title"],Group["swfadmins"],Group["sysadmins"]] ;
                 }
 			 	file {
 					"/home/$title" :
