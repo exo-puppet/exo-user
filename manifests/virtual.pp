a2c1931688c8a1f631cc56e539c18e07af5a1af1
Arnaud Héritier <aheritier@apache.org>
2011-10-26 16:28:56 +0200

    Cleanup users management and improve groups management

--- manifests/virtual.pp
+++ manifests/virtual.pp
@@ -2,119 +2,111 @@
 #
 # People accounts of interest as virtual resources
 class user::virtual {
-	 	group {	"sysadmins":
-				gid     =>   5000,
-				ensure => "present" ;
-		} 
 	 	group {	"user":
-				gid     =>   5001,
+				gid     =>   6000,
+				allowdupe => false,
 				ensure => "present" ;
 		} 
-	 	group {	"swfadmins":
-				gid     =>   5002,
+	 	group {	"swf":
+				gid     =>   6001,
+				allowdupe => false,
 				ensure => "present" ;
 		} 
-        define sysadmin ($uid,$gid,$pass,$realname="",$email="",$sshkey="",$ensure="present") {
+	 	group {	"qaf":
+				gid     =>   6002,
+				allowdupe => false,
+				ensure => "present" ;
+		} 
+	 	group {	"itop":
+				gid     =>   6003,
+				allowdupe => false,
+				ensure => "present" ;
+		} 
+	 	group {	"sysadmin":
+				gid     =>   6004,
+				allowdupe => false,
+				ensure => "present" ;
+		} 
+        define user ($uid,$gid,$pass,$groups,$realname="",$email="",$sshkey="",$ensure="present") {
+        	    # Default groups for all accounts
+        	    $default_groups = ["user"]
+			    # This case statement will allow disabling an account by passing
+			    # ensure => absent, to set the home directory ownership to root.
+			    case $ensure {
+			        present: {
+			            $home_owner = $title
+			            $home_group = $title
+			        }
+			        default: {
+			            $home_owner = "root"
+			            $home_group = "root"
+			        }
+			    }        	
 			 	group {	$title:
 						gid     =>      $gid,
+						allowdupe => false,
 						ensure => $ensure ;
 				} 
+			    # Ordering of dependencies, just in case
+			    case $ensure {
+			        present: { User <| title == "$title" |> { require => Group["$title"] } }
+			        absent: { Group <| title == "$title" |> { require => User["$title"] } }
+			    }				
                 user { $title:
                         ensure  =>      $ensure,
                         uid     =>      $uid,
                         gid     =>      $gid,
-                        groups  =>      "sysadmins",
+                        groups  =>      [$groups,$default_groups],
+                        membership =>   inclusive, # specify the complete list of groups (remove not listed here)
                         shell   =>      "/bin/bash",
                         home    =>      "/home/$title",
                         comment =>      $realname,
                         password =>     $pass,
                         managehome =>   true,
-                        require => [Group["$title"],Group["sysadmins"],] ;
+                        require =>      [Group[$groups,$default_groups]] ;
                 }
 			 	file {
 					"/home/$title" :
-						ensure => directory,
+						ensure => $ensure ? {
+						                        present => directory,
+						                        absent  => absent,
+						                    },
 						require => User["$title"],
-						owner => "$title",
-						group => "$title" ;
+						owner => $home_owner,
+						group => $home_group ;
 				}
                 if ( $email != "" ) {
 				 	file {
 						"/home/$title/.forward" :
-							ensure => file,
+							ensure => $ensure ? {
+						                        present => file,
+						                        absent  => absent,
+						                    },
 							content => "$email",
 							require => User["$title"],
-							owner => "$title",
-							group => "$title" ;
+							owner => "$home_owner",
+							group => "$home_group" ;
 					}                	
                 }
                 if ( $sshkey != "" ) {
-					ssh_authorized_key { $title:
-					    ensure  =>      "present",
-					    type    =>      "ssh-rsa",
-					    key     =>      "$sshkey",
-					    user    =>      "$title",
-					    require =>      [User["$title"],File["/home/$title/.ssh"]],
-					    name    =>      "$title",
-					}
 					file {
 						"/home/$title/.ssh" :
-							ensure => directory,
-							require => File["/home/$title"],
-							owner => "$title",
-							group => "$title" ;
+							ensure => $ensure ? {
+							                        present => directory,
+							                        absent  => absent,
+							                    },
+							force =>  true,
+							require => [User["$title"],File["/home/$title"]],
+							owner => "$home_owner",
+							group => "$home_group" ;
 					}
-                }
-        }
-        define swfadmin ($uid,$gid,$pass,$realname="",$email="",$sshkey="",$ensure="present") {
-			 	group {	$title:
-						gid     =>      $gid,
-						ensure => $ensure ;
-				} 
-                user { $title:
-                        ensure  =>      $ensure,
-                        uid     =>      $uid,
-                        gid     =>      $gid,
-                        groups  =>      ["swfadmins","sysadmins"],
-                        shell   =>      "/bin/bash",
-                        home    =>      "/home/$title",
-                        comment =>      $realname,
-                        password =>     $pass,
-                        managehome =>   true,
-                        require => [Group["$title"],Group["swfadmins"],Group["sysadmins"]] ;
-                }
-			 	file {
-					"/home/$title" :
-						ensure => directory,
-						require => User["$title"],
-						owner => "$title",
-						group => "$title" ;
-				}
-                if ( $email != "" ) {
-				 	file {
-						"/home/$title/.forward" :
-							ensure => file,
-							content => "$email",
-							require => User["$title"],
-							owner => "$title",
-							group => "$title" ;
-					}                	
-                }
-                if ( $sshkey != "" ) {
 					ssh_authorized_key { $title:
-					    ensure  =>      "present",
+					    ensure  =>      $ensure,
 					    type    =>      "ssh-rsa",
 					    key     =>      "$sshkey",
-					    user    =>      "$title",
+					    user    =>      "$home_owner",
 					    require =>      [User["$title"],File["/home/$title/.ssh"]],
 					    name    =>      "$title",
-					}
-					file {
-						"/home/$title/.ssh" :
-							ensure => directory,
-							require => File["/home/$title"],
-							owner => "$title",
-							group => "$title" ;
 					}
                 }
         }
